ab9cbf46f3b88eef7fd05fd9c06a3f91bdde7e4c
Gustavo Moura <gustavo.dart@hotmail.com>
2024-08-13 14:01:09 -0300

    Formulario para agendamento de mentori_v1
    Estudo de mentoria com formulario para preenchimento de campos de acesso, data e hora para agendamento de mentoria com profissional.(versão de estudo)

---
+++ Formulário_interm/script.js
@@ -0,0 +1,40 @@
+//1. Declare uma variável de nome weight
+
+let weight
+
+//2. Que tipo de dado é a variável acima?
+
+// console.log(typeof weight) 
+
+/*3. Declare uma variável e atribua valores para cada
+um dos dados:
+        *name: String
+        *age: Number(integer)
+        *stars: Number (float)
+        *isSubscribed: Boolean
+
+*/
+
+let name = 'Mario'
+let age = 32
+let stars = 15.3
+let isSubscribed
+
+    if(isSubscribed === true){
+        console.log(isSubscribed)
+    }
+
+    isSubscribed = true
+
+
+console.log(`Olá, eu sou o ${name}, tenho ${age} anos, possuo ${stars} estrelas e meu status de inscrição é: ${isSubscribed === true}
+
+
+`)
+
+let student = {
+    name: 'Mario',
+    age: 32,
+    stars: 15.3,
+    isSubscribed: true
+}
